e5aa9e980dba8b001dc702e28385c42a6166959d
slinkz2 <157033055+slinkz2@users.noreply.github.com>
2024-03-21 22:18:44 +0800

    update font
    kairu

--- lib/main.dart
+++ lib/main.dart
@@ -12,43 +12,51 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF2C2C2C),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(20),
-            height: 200,
-            width: double.maxFinite,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Color(0xFF8EACCD),
+      // backgroundColor: Color(0xFF2C2C2C),
+      body: Container(decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('../assets/mountain_vector.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              height: 300,
+              width: double.maxFinite,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                // color: Color(0xFF8EACCD),
+              ),
+              child: Text('TriviaTime App',style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Autour',
+                fontSize: 35,
+              ),),
             ),
-            child: Text('TriviaTime App',style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-            ),),
-          ),
-          Expanded(
-            child: Center(
-            child: SizedBox(
-              height: 40,
-              width: 200,
-              child: ElevatedButton(
-                child: Text('Start Trivia', style: TextStyle(color: Colors.white),),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF8EACCD),
-                ),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (BuildContext context) => StartTrivia())
-                  );
-                },
+            Expanded(
+              child: Center(
+              child: SizedBox(
+                height: 50,
+                width: 300,
+                child: ElevatedButton(
+                  child: Text('Start Trivia', style: TextStyle(color: Colors.white, fontFamily: 'Imprima',fontSize: 25),),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF8EACCD),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (BuildContext context) => StartTrivia())
+                    );
+                  },
 
+                ),
               ),
             ),
-          ),
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
